8762a8366feee815db91b23f2d815c872c869ee4
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2012-10-19 11:50:29 -0400

    clean project

--- conf/stores.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :judge)
-
-;;; Multiple stores may be defined. The last defined store will be the
-;;; default.
-(defstore *judge-store* :prevalence
-  (merge-pathnames (make-pathname :directory '(:relative "data"))
-		   (asdf-system-directory :judge)))
-
